7311c846b73ea3498f9b16822a695bc9cf0a012b
Brendan Whitaker <whitaker.213@osu.edu>
2019-12-18 14:30:27 +0000

    Added a 'plotplotplot' package folder.
    Refactored the entire repository. Moved fonts and prototypes into the plotplotplot folder.
    Changed the import structure to something more standard. Added a .gitignore. Got rid of
    some old output files. Added a requirements file.

--- plotplotplot/preprocessing.py
+++ plotplotplot/preprocessing.py
@@ -3,10 +3,11 @@ import json
 import numpy as np
 import pandas as pd
 
-#DATA PREPROCESSING
+# DATA PREPROCESSING
+
 
 def read_csv(file_path):
- 
+
     print("Reading from file:", file_path)
 
     df = pd.read_csv(file_path)
@@ -20,11 +21,11 @@ def read_csv(file_path):
         column_counts.append(1)
         i += 1
 
-    # Iterate over column split, and create a seperate DataFrame for 
-    # each subplot. Add the subplot names to `ylabels`. 
+    # Iterate over column split, and create a seperate DataFrame for
+    # each subplot. Add the subplot names to `ylabels`.
     ylabel = ""
     ylabels = []
-    for i,count in enumerate(column_counts):
+    for i, count in enumerate(column_counts):
         key_list = []
         if count == 1:
             ylabels.append(keys[i])
@@ -39,15 +40,17 @@ def read_csv(file_path):
             ylabels.append("".join(words[:-1]))
             dfs.append(df[key_list])
     print("Generating", len(dfs), "subplots.")
-    
+
     return dfs, ylabels, column_counts
+
 
 def read_json(file_path, phase):
 
-    phases = ['train', 'validate', 'test']
+    phases = ["train", "validate", "test"]
     assert phase in phases
-    
+
     print("Reading from file:", file_path)
+
     def isfloat(x):
         try:
             a = float(x)
@@ -65,15 +68,15 @@ def read_json(file_path, phase):
         else:
             return a == b
 
-    with open(file_path, encoding='utf-8') as json_file:
+    with open(file_path, encoding="utf-8") as json_file:
         json_data = json.load(json_file)
-        
+
         phase_key = phase + "Results"
 
         row_list = []
         log_list = json_data[phase_key]
 
-        # Construct train array. 
+        # Construct train array.
         for i, stepdict in enumerate(log_list):
             row = []
             for key, val in stepdict.items():
@@ -83,33 +86,33 @@ def read_json(file_path, phase):
                     row.append(float(val))
                 else:
                     row.append(val)
-            row_list.append(row) 
+            row_list.append(row)
 
         log_df = pd.DataFrame(log_list)
         keys = list(log_df.columns)
-        log_df['index'] = log_df.index
-        log_df['index'] = log_df['index'].apply(lambda x: x*10)
-       
+        log_df["index"] = log_df.index
+        log_df["index"] = log_df["index"].apply(lambda x: x * 10)
+
     dfs = []
 
-    # This column_counts generation process assumes 
-    # `top5` always follows `top1` in keys. 
+    # This column_counts generation process assumes
+    # `top5` always follows `top1` in keys.
     column_counts = []
     i = 0
     for i in range(len(keys)):
         key = keys[i]
-        if key == 'top1':
+        if key == "top1":
             column_counts.append(2)
-        elif key != 'top5':
+        elif key != "top5":
             column_counts.append(1)
         i += 1
 
-    # Iterate over column split, and create a seperate DataFrame for 
-    # each subplot. Add the subplot names to `ylabels`. 
+    # Iterate over column split, and create a seperate DataFrame for
+    # each subplot. Add the subplot names to `ylabels`.
     ylabel = ""
     ylabels = []
-    for i,count in enumerate(column_counts):
-        key_list = ['index']
+    for i, count in enumerate(column_counts):
+        key_list = ["index"]
         if count == 1:
             ylabels.append(keys[i])
             key_list.append(keys[i])
@@ -123,5 +126,5 @@ def read_json(file_path, phase):
             ylabels.append("".join(words[:-1]))
             dfs.append(log_df[key_list])
     print("Generating", len(dfs), "subplots.")
-    
+
     return dfs, ylabels, column_counts
